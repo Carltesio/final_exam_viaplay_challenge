4356123699500a74a97d627ded0ab3739d5d3835
carlos <carlos.delgado@hotmail.it>
2020-04-22 17:04:57 +0200

    scaffolded react app

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="ui fixed inverted menu">
+      <div className="ui container">
+        <Link id="header" className="header item" to="/">
+          My Portfolio
+        </Link>  
+      </div>
+    </nav>
+
+    
+  );
+};
+
+export default Header;
